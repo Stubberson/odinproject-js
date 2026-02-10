cec6eb92239b65e052d7796c2207b31d71ed1849
Stubberson <tuukka.j.korpela@gmail.com>
2026-02-10 18:11:37 +0100

    Finish the last JS foundation exercise
    I didn't know about the "nullish" operator at first and did it with
    a basic if clause in my original solution. The nullish operator saves
    a few lines.

--- javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
+++ javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    // Add missing data
+    array.forEach(person => {
+        // Nullish operator only does the assignment if the left side is "nullish" (undefined or null)
+        person['yearOfDeath'] ??= new Date().getFullYear()
+    })
+    // Sort the modified array in decreasing order and return the first element (oldest)
+    return array.sort((a, b) => (b['yearOfDeath'] - b['yearOfBirth']) - (a['yearOfDeath'] - a['yearOfBirth']))[0]
 };
 
 // Do not edit below this line
