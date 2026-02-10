bf9259df01487debd3a397707e1b04e23d406014
Stubberson <tuukka.j.korpela@gmail.com>
2026-02-10 16:44:53 +0100

    Finish fibonacci exercise

--- javascript-exercises/foundations/14_fibonacci/fibonacci.js
+++ javascript-exercises/foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = parseInt(a)
+    if (a > 0) {
+        // Create the fibonacci sequence
+        const sequenceLength = a  // We don't have to go any further than the given number
+        const sequence = [1, 1]  // Our sequence is 1, 1, 2, 3, 5, 8 etc.            
+        for (let i = 2; i < sequenceLength; i++) {
+            sequence[i] = sequence[i - 1] + sequence[i - 2]
+        }
+        // Return the value at queried index
+        return sequence[a - 1]
+    } else if (a === 0) {
+        return 0
+    } else {
+        return 'OOPS'
+    }
 };
 
 // Do not edit below this line
